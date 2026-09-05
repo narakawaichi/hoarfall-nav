9ef9b797ed3a2527a5acac2d0125a277390e15a2
阿晨 <achen@hoarfall.com>
2026-09-05 14:38:38 +0800

    feat: 友链新增 欣文网 + 开心不开心

--- data/friends.ts
+++ data/friends.ts
@@ -10,4 +10,20 @@ export const friendsData: Friend[] = [
     avatar: "https://avatars.githubusercontent.com/u/90437691?v=4",
     themeColor: "#8b5cf6",
   },
+  {
+    id: "xinenw",
+    name: "欣文网",
+    url: "https://www.xinenw.com",
+    description: "一个致力于为未来青年和新时代大学生打造的优质内容平台！",
+    avatar: "https://www.xinenw.com/wp-content/uploads/2026/03/20241122032115460.jpg",
+    themeColor: "#3b82f6",
+  },
+  {
+    id: "kaixin",
+    name: "开心不开心",
+    url: "https://www.xinenw.cn",
+    description: "个人特色IP内容！",
+    avatar: "https://www.xinenw.cn/wp-content/uploads/2025/12/KX.jpg",
+    themeColor: "#f97316",
+  },
 ];
